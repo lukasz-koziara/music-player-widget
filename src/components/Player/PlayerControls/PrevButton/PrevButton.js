619b649e3840274c2dbff70b5f691d6a2f576709
Koza88 <lukasz.koziara88@gmail.com>
2018-08-09 20:45:54 +0200

    added player controls with styling, added background

--- src/components/Player/PlayerControls/PrevButton/PrevButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import classes from '../PlayerControls.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-const prevButton = (props) => (
-    <div className={classes.Prev}><FontAwesomeIcon icon={"step-backward"}/>{props.label}</div>
-);
-
-export default prevButton
